e8ff8e0e884593c1e9711ab6c19679217953f1cf
dreamhel <dreamhel@163.com>
2017-01-12 00:46:51 +0800

    add delete class

---
+++ src/main/java/mongo/events/CascadeDeleteCallBack.java
@@ -0,0 +1,7 @@
+package mongo.events;
+
+/**
+ * Created by g00216500 on 2017/1/12.
+ */
+public class CascadeDeleteCallBack {
+}
---
+++ src/main/java/mongo/events/CascadeSaveCallback.java
@@ -0,0 +1,49 @@
+package mongo.events;
+
+import org.springframework.data.annotation.Reference;
+import org.springframework.data.mapping.model.MappingException;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.util.ReflectionUtils;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/12/30.
+ */
+public class CascadeCallback implements ReflectionUtils.FieldCallback {
+    private Object source;
+    private MongoTemplate mongoTemplate;
+
+    public CascadeCallback(Object source, MongoTemplate mongoTemplate) {
+        super();
+        this.source = source;
+        this.mongoTemplate = mongoTemplate;
+    }
+
+    @Override
+    public void doWith(Field field) throws IllegalArgumentException, IllegalAccessException {
+
+        ReflectionUtils.makeAccessible(field);
+
+        if (field.isAnnotationPresent(DBRef.class) && field.isAnnotationPresent(DBCascade.class)) {
+            Class<?> sourceClass = field.get(source).getClass();
+            Object fieldValue = field.get(source);
+            if (List.class.isAssignableFrom(sourceClass))  //arry references
+            {
+                for (Object obj : (List<?>) fieldValue) {
+                    if (obj.getClass().isAnnotationPresent(Document.class)) {
+                        mongoTemplate.save(obj);
+                    }
+                }
+
+            } else {
+                       mongoTemplate.save(field);
+            }
+        }
+    }
+}
